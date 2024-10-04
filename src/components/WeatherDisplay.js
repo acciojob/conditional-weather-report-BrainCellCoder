2c9a07685760d903a33993a8f8bac33a9c8820cb
BrainCellCoder <00abhisheksharma1999@gmail.com>
2024-10-04 08:53:05 +0530

    done

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const WeatherDisplay = ({ temperature, conditions }) => {
   return (
-    <>
+    <p>
       <span style={{ color: temperature > 20 ? "red" : "blue" }}>
         Temperature: {temperature}
       </span>
       <span>Conditions: {conditions}</span>
-    </>
+    </p>
   );
 };
 
